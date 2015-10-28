59acbbef1f1bf69620fef78f89e3306b06670bfe
Haley Blackburn <haleyblackburn@Haleys-MacBook-Pro.local>
2015-10-28 11:18:52 -0500

    added route

--- app/src/app/app.js
+++ app/src/app/app.js
@@ -5,12 +5,20 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 var platypus_1 = require('platypus');
 var home_vc_1 = require('../viewcontrols/home/home.vc');
+var calendar_vc_1 = require('../viewcontrols/calendar/calendar.vc');
+var contact_vc_1 = require('../viewcontrols/contact/contact.vc');
+var login_vc_1 = require('../viewcontrols/login/login.vc');
+var services_vc_1 = require('../viewcontrols/services/services.vc');
 var MyApp = (function (_super) {
     __extends(MyApp, _super);
     function MyApp(router) {
         _super.call(this);
         router.configure([
-            { pattern: '', view: home_vc_1.default }
+            { pattern: '', view: home_vc_1.default },
+            { pattern: '/services', view: services_vc_1.default },
+            { pattern: '/contact', view: contact_vc_1.default },
+            { pattern: '/login', view: login_vc_1.default },
+            { pattern: '/calendar', view: calendar_vc_1.default }
         ]);
     }
     MyApp.prototype.error = function (ev) {
--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,5 +1,10 @@
 import {App, events, register, routing} from 'platypus';
 import HomeViewControl from '../viewcontrols/home/home.vc';
+import CalendarViewControl from '../viewcontrols/calendar/calendar.vc';
+import ContactViewControl from '../viewcontrols/contact/contact.vc';
+import LoginViewControl from '../viewcontrols/login/login.vc';
+import ServicesViewControl from '../viewcontrols/services/services.vc';
+
 
 
 export default class MyApp extends App {
@@ -7,7 +12,11 @@ export default class MyApp extends App {
         super();
 
         router.configure([
-            { pattern: '', view: HomeViewControl }
+            { pattern: '', view: HomeViewControl },
+            {pattern:'/services', view:ServicesViewControl },
+            {pattern: '/contact', view: ContactViewControl },
+            {pattern: '/login', view:LoginViewControl},
+            {pattern: '/calendar', view: CalendarViewControl}
         ]);
     }
 
