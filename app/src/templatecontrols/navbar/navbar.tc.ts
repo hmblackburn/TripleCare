00b79ed057da9d0d0c38aeabcc575e736a249892
Haley Blackburn <haleyblackburn@Haleys-MacBook-Pro.local>
2015-10-28 14:49:56 -0500

    node modules deleted

--- app/src/templatecontrols/navbar/navbar.tc.ts
+++ app/src/templatecontrols/navbar/navbar.tc.ts
@@ -1,4 +1,10 @@
-import {register, ui} from 'platypus';
+import {controls, events, register, ui, web} from 'platypus';
+import HomeViewControl from '../../viewcontrols/home/home.vc';
+import CalendarViewControl from '../../viewcontrols/calendar/calendar.vc';
+import ContactViewControl from '../../viewcontrols/contact/contact.vc';
+import LoginViewControl from '../../viewcontrols/login/login.vc';
+import ServicesViewControl from '../../viewcontrols/services/services.vc';
+import {DrawerController} from 'platypusui';
 
 export default class NavbarTemplateControl extends ui.TemplateControl {
     templateString: string = require('./navbar.tc.html');
