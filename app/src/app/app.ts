e39985f1b9b521556862fd762053d9e99283975d
Daniel Boone <DanielBoone@Daniels-MacBook-Pro.local>
2015-10-29 09:32:19 -0500

    Reigister Push

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,9 +1,11 @@
 import {App, events, register, routing} from 'platypus';
 import HomeViewControl from '../viewcontrols/home/home.vc';
+import RegisterViewControl from '../viewcontrols/register/register.vc';
 import CalendarViewControl from '../viewcontrols/calendar/calendar.vc';
 import ContactViewControl from '../viewcontrols/contact/contact.vc';
 import LoginViewControl from '../viewcontrols/login/login.vc';
 import ServicesViewControl from '../viewcontrols/services/services.vc';
+
 
 
 
@@ -13,10 +15,11 @@ export default class MyApp extends App {
 
         router.configure([
             { pattern: '', view: HomeViewControl },
-            {pattern:'/services', view:ServicesViewControl },
-            {pattern: '/contact', view: ContactViewControl },
-            {pattern: '/login', view:LoginViewControl},
-            {pattern: '/calendar', view: CalendarViewControl}
+            { pattern: 'register', view: RegisterViewControl },
+            { pattern: '/services', view: ServicesViewControl },
+            { pattern: '/contact', view: ContactViewControl },
+            { pattern: '/login', view: LoginViewControl },
+            { pattern: '/calendar', view: CalendarViewControl }
         ]);
     }
 
@@ -28,3 +31,4 @@ export default class MyApp extends App {
 register.app('app', MyApp, [
     routing.Router
 ]);
+
--- app/src/templatecontrols/navbar/navbar.tc.ts
+++
@@ -1,7 +0,0 @@
-import {register, ui} from 'platypus';
-
-export default class NavbarTemplateControl extends ui.TemplateControl {
-    templateString: string = require('./navbar.tc.html');
-}
-
-register.control('navbar', NavbarTemplateControl);
