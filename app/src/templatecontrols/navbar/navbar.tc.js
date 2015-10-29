39e62ca17e607dc1aee63ddd5eed81b4d1a80e1d
Ethan Fairweather <Ethan-Fairweather@Ethans-MacBook-Pro.local>
2015-10-29 11:15:41 -0500

    added navbar html and part of the ts file

--- app/src/templatecontrols/navbar/navbar.tc.js
+++ app/src/templatecontrols/navbar/navbar.tc.js
@@ -4,12 +4,37 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var platypus_1 = require('platypus');
+var home_vc_1 = require('../../viewcontrols/home/home.vc');
+var calendar_vc_1 = require('../../viewcontrols/calendar/calendar.vc');
+var contact_vc_1 = require('../../viewcontrols/contact/contact.vc');
+var login_vc_1 = require('../../viewcontrols/login/login.vc');
+var services_vc_1 = require('../../viewcontrols/services/services.vc');
 var NavbarTemplateControl = (function (_super) {
     __extends(NavbarTemplateControl, _super);
     function NavbarTemplateControl() {
         _super.apply(this, arguments);
         this.templateString = require('./navbar.tc.html');
+        this.context = {
+            showNavbar: true,
+            homeView: home_vc_1.default,
+            calView: calendar_vc_1.default,
+            contactView: contact_vc_1.default,
+            servView: services_vc_1.default,
+            loginView: login_vc_1.default,
+            loggedin: false
+        };
     }
+    NavbarTemplateControl.prototype.initialize = function () {
+        var _this = this;
+        this.on('navigated', function (ev, utils) {
+            _this.drawerController.control.close();
+        });
+    };
+    NavbarTemplateControl.prototype.toggleLogin = function () {
+        this.context.loggedin = !this.context.loggedin;
+    };
+    NavbarTemplateControl.prototype.logOut = function () {
+    };
     return NavbarTemplateControl;
 })(platypus_1.ui.TemplateControl);
 Object.defineProperty(exports, "__esModule", { value: true });
--- app/src/templatecontrols/navbar/navbar.tc.ts
+++ app/src/templatecontrols/navbar/navbar.tc.ts
@@ -8,6 +8,31 @@ import {DrawerController} from 'platypusui';
 
 export default class NavbarTemplateControl extends ui.TemplateControl {
     templateString: string = require('./navbar.tc.html');
+    drawerController: controls.INamedElement<HTMLDivElement, DrawerController>;
+
+    context: any = {
+      showNavbar: true,
+      homeView: HomeViewControl,
+      calView: CalendarViewControl,
+      contactView: ContactViewControl,
+      servView: ServicesViewControl,
+      loginView:LoginViewControl,
+      loggedin: false
+    }
+
+    initialize(): void {
+      this.on('navigated', (ev: events.DispatchEvent, utils: web.UrlUtils) => {
+        this.drawerController.control.close();
+      });
+    }
+
+    toggleLogin() {
+        this.context.loggedin = !this.context.loggedin;
+    }
+    logOut(){
+      //log user out
+      
+    }
 }
 
 register.control('navbar', NavbarTemplateControl);
