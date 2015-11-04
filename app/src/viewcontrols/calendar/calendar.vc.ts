f6009ef044501381706e8aec651dd9110237b4d9
Daniel Boone <DanielBoone@Daniels-MacBook-Pro.local>
2015-11-04 11:21:43 -0600

    Calendar TS Update

--- app/src/viewcontrols/calendar/calendar.vc.ts
+++ app/src/viewcontrols/calendar/calendar.vc.ts
@@ -4,7 +4,9 @@ import BaseViewControl from '../base/base.vc';
 export default class CalendarViewControl extends BaseViewControl {
     templateString: string = require('./calendar.vc.html');
 
-    context: any = {};
+    context: any = {
+        
+    };
 }
 
 register.viewControl('calendar-vc', CalendarViewControl);
