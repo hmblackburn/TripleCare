6e9247a8d2742c43f06e9da710bd5cc672dee87b
Haley Blackburn <haleyblackburn@Haleys-MacBook-Pro.local>
2015-11-05 15:22:00 -0600

    login works

--- app/src/viewcontrols/register/register.vc.ts
+++ app/src/viewcontrols/register/register.vc.ts
@@ -1,35 +1,33 @@
+/// <reference path="../../../../typings/parse/parse.d.ts"/>
 import {register} from 'platypus';
 import BaseViewControl from '../base/base.vc';
 import HomeViewControl from '../home/home.vc';
+import CalendarViewControl from '../calendar/calendar.vc';
 import UserRepository from '../../repositories/user/user.repo';
 
 export default class RegisterViewControl extends BaseViewControl {
     templateString: string = require('./register.vc.html');
-
+    context = {
+        password: '',
+        error: '',
+        username: ''
+    }
+    
     constructor(private userRepository: UserRepository) {
       super();
     }
 
-  context: contexts.IRegister = {
-      firstname: '',
-      lastname: '',
-      email: '',
-      password: '',
-      error: ''
+  login(){
+    Parse.User.logIn(this.context.username, this.context.password)
+    .then((success) => {
+        this.navigator.navigate(CalendarViewControl);
+    });
+    
   }
   
-
-  register(): void {
-      this.context.error = '';
-      this.userRepository.register(this.context.email,
-          this.context.password,
-          this.context.firstname,
-          this.context.lastname)
-      .then((success) => {
-          this.navigator.navigate(HomeViewControl);
-      }).catch((error) => {
-          this.context.error = error;
-      });
+  logOut(){
+    Parse.User.logOut();
   }
+
 }
 register.viewControl('register-vc', RegisterViewControl, [UserRepository]);
