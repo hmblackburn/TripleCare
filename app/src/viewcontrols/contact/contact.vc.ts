a81cdd072afd1ad3cfc63a0f93d371fdea9c6b3f
Daniel Boone <DanielBoone@Daniels-MacBook-Pro.local>
2015-11-05 13:41:15 -0600

    Pulling for New Nav bar

--- app/src/viewcontrols/contact/contact.vc.ts
+++ app/src/viewcontrols/contact/contact.vc.ts
@@ -4,7 +4,17 @@ import BaseViewControl from '../base/base.vc';
 export default class ContactViewControl extends BaseViewControl {
     templateString: string = require('./contact.vc.html');
 
-    context: any = {};
+    context: any = {
+        customer: {
+            name: '',
+            email: '',
+            comment: '',
+        }
+    };
+    
+    pushEntry() {
+        //this.context.customer is an object where name, email, comment and bundled together. Can be sent off to a repo/service
+    }
 }
 
 register.viewControl('contact-vc', ContactViewControl);
